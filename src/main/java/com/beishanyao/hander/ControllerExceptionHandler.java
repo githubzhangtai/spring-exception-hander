240a1d0870dd5fee28078321e46bf09253ce142a
kimizhang <kimizhang@swireproperties.com>
2020-01-10 11:13:26 +0800

    0110-自定义异常，排除需要拦截的异常状态

--- src/main/java/com/beishanyao/hander/ControllerExceptionHandler.java
+++ src/main/java/com/beishanyao/hander/ControllerExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.beishanyao.hander;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,9 +19,16 @@ import javax.servlet.http.HttpServletRequest;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e){
+    public ModelAndView exceptionHandler(HttpServletRequest request,Exception e) throws Exception {
         log.error("Request URL : {}, Exception : {}",request.getRequestURL(),e);
 
+        /**
+         * if模块
+         * 标注了 ResponseStatus 状态的异常，不在此处统一处理，直接抛出异常，让 springboot 本身处理
+         */
+        if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
+            throw e;
+        }
         ModelAndView mv = new ModelAndView();
         mv.addObject("url",request.getRequestURL());
         mv.addObject("exception",e);
--- src/main/java/com/beishanyao/web/IndexController.java
+++ src/main/java/com/beishanyao/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.beishanyao.web;
 
+import com.beishanyao.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
+@Slf4j
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
-        int i = 9/0;
+//        int i = 9/0;
+        String test = null;
+        if(test == null){
+            log.error("test不存在");
+            throw new NotFoundException("test不存在");
+        }
         return "index";
     }
 }
